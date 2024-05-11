0ed3c63315a7ee55a5881bf49f0fec5d0b5185f2
Qiu Yan ping <2740123221@qq.com>
2024-05-11 21:06:25 +0800

    测试修改功能1，新增功能2

--- test/src/operation1.java
+++ test/src/operation1.java
@@ -1,5 +1,6 @@
 public class operation1 {
     public static void main(String[] args) {
         System.out.println("功能1");
+        System.out.println("功能1修改");
     }
 }
---
+++ test/src/operation2.java
@@ -0,0 +1,5 @@
+public class operation2 {
+    public static void main(String[] args) {
+        System.out.println("功能2");
+    }
+}
